a98577e7af1959d27317dea6fbff908a9f1c4399
pareshpatil98 <pareshpatil131298@gmail.com>
2023-02-04 18:15:31 +0530

    root set to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/info'
   resources :products
+  root to:"products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/Products")
