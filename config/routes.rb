ea75154a886c530c46d3f6007b0b6bd3dcfe7e30
smitaRbhamare <bhamaresmita29@gmail.com>
2023-02-04 17:02:43 +0530

    Updated page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/Products")
   # root "articles#index"
 end
